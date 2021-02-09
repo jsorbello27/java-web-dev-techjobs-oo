6ae4381aa5f07c82a38e2ae4aa8b9143f5bfef71
jsorbello27 <j.sorbello27@gmail.com>
2021-02-08 19:17:14 -0600

    fixed null issue

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
-        Job job4 = new Job("poopie face", null , new Location("Toilet"), new PositionType("Turd Burgler"), new CoreCompetency("#2"));
+
 
 
 
@@ -17,7 +17,7 @@ public class Main {
         jobs.add(job1);
         jobs.add(job2);
         jobs.add(job3);
-        jobs.add(job4);
+
 
         for (Job job : jobs){
             System.out.println(job);
